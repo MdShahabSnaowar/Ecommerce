5e631094fa22ea399167d74d7400054f1a6e30d5
alkhanazhar <khan.azhar.saf@gmail.com>
2025-07-07 11:12:30 +0530

    auth in the medicals

--- routes/medical.js
+++ routes/medical.js
@@ -8,8 +8,9 @@ const {
 } = require("../controllers/medical.controllers");
 
 const router = express.Router();
+const authAdmin = require("../middleware/authAdmin");
 
-router.post("/create", createMedicalItem);
+router.post("/create", authAdmin, createMedicalItem);
 router.get("/", getAllMedicalItems);
 router.get("/:id", getMedicalItemById);
 router.put("/:id", updateMedicalItem);
