8df135074a0d425ef505d94a07369ab77f473cc3
MdShahabSnaowar <shahabsanowar786@gmail.com>
2025-07-08 16:42:04 +0530

    updated code

--- routes/groceryProductRoutes.js
+++ routes/groceryProductRoutes.js
@@ -14,8 +14,8 @@ router.post("/create", upload.single("image"), productController.createProduct);
 // }, productController.createProduct);
 
 router.get("/get-all", productController.getAllProducts);
-router.get("/get-by-id/:id", productController.getProductById);
-router.get("/get-by-subcategory/:subcategoryId", productController.getProductsBySubcategory);
+// router.get("/get-by-id/:id", productController.getProductById);
+router.get("/:subcategoryId", productController.getProductsBySubcategory);
 
 
 router.put("/update/:id", upload.single("image"), productController.updateProduct);
